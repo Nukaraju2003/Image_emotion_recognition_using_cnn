fadd8ecda53647f1f8065fb2275058ce4b5a8eca
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-07-21 13:09:23 +0530

    video

--- src/emotions.py
+++ src/emotions.py
@@ -8,6 +8,14 @@ from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# change
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout, Flatten
+# from keras.layers import Conv2D
+# from keras.optimizers import Adam
+# from keras.layers import MaxPooling2D
+# from keras.preprocessing.image import ImageDataGenerator
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -75,6 +83,7 @@ model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
+
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
@@ -109,20 +118,27 @@ elif mode == "display":
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
     # start the webcam feed
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
+    
+    #sample video
+    cap = cv2.VideoCapture("src\\Video\\videoplayback.mp4")
     while True:
         # Find haar cascade to draw bounding box around face
         ret, frame = cap.read()
+        frame = cv2.resize(frame, (1280, 720))
         if not ret:
             break
         facecasc = cv2.CascadeClassifier('src\\haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
-
+        
+        # take each face available on the camera and preprocess it
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            
+            # predict the emotions
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
